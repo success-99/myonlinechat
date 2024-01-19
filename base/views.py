97579971ef5c7ea849f6f7261dba4caf722e48d8
mbp13 <shaxobiddina20@gmail.com>
2024-01-19 22:35:37 +0500

    deploy project

--- base/views.py
+++ base/views.py
@@ -6,21 +6,20 @@ from agora_token_builder import RtcTokenBuilder
 from .models import RoomMember
 import json
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseNotFound
 
-
-
-# Create your views here.
 
 def lobby(request):
     return render(request, 'base/lobby.html')
+
 
 def room(request):
     return render(request, 'base/room.html')
 
 
 def getToken(request):
-    appId = "YOUR APP ID"
-    appCertificate = "YOUR APP CERTIFICATE"
+    appId = "c5a6894fa2f94630af04737ee8d5febb"
+    appCertificate = "dacad5ac5652452787805f323ef5999b"
     channelName = request.GET.get('channel')
     uid = random.randint(1, 230)
     expirationTimeInSeconds = 3600
@@ -42,19 +41,26 @@ def createMember(request):
         room_name=data['room_name']
     )
 
-    return JsonResponse({'name':data['name']}, safe=False)
+    return JsonResponse({'name': data['name']}, safe=False)
 
 
 def getMember(request):
     uid = request.GET.get('UID')
     room_name = request.GET.get('room_name')
+    try:
+        member = RoomMember.objects.get(uid=uid, room_name=room_name)
+        name = member.name
+        return JsonResponse({'name': name}, safe=False)
+    except RoomMember.DoesNotExist:
+        return HttpResponseNotFound('RoomMember topilmadi')
 
-    member = RoomMember.objects.get(
-        uid=uid,
-        room_name=room_name,
-    )
-    name = member.name
-    return JsonResponse({'name':member.name}, safe=False)
+    # member = RoomMember.objects.get(
+    #     uid=uid,
+    #     room_name=room_name,
+    # )
+    # name = member.name
+    # return JsonResponse({'name': member.name}, safe=False)
+
 
 @csrf_exempt
 def deleteMember(request):
--- static/js/streams.js
+++ static/js/streams.js
@@ -1,10 +1,12 @@
 
-const APP_ID = 'YOUR APP ID'
+const APP_ID = 'c5a6894fa2f94630af04737ee8d5febb'
 const TOKEN = sessionStorage.getItem('token')
+console.log('TOKEN:', TOKEN);
 const CHANNEL = sessionStorage.getItem('room')
-let UID = sessionStorage.getItem('UID')
+let UID = Number((sessionStorage.getItem('UID'));
 
 let NAME = sessionStorage.getItem('name')
+const AgoraRTC = require('AgoraRTC_N-4.20.0.js');
 
 const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
 
