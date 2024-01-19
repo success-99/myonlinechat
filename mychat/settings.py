a7b906fd6633e24cd3ae5be57dc612b658544a7d
mbp13 <shaxobiddina20@gmail.com>
2024-01-19 22:36:13 +0500

    deploy project settings

--- mychat/settings.py
+++ mychat/settings.py
@@ -123,6 +123,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+STATIC_ROOT = 'static-files'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
